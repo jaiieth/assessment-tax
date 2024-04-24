59b19b2881b1bbe414e76cecae4fa66bfa455dfa
Earth Varis <varis.a@siamraj.com>
2024-04-25 00:35:17 +0700

    feat: include tax levels in response

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,6 +1,8 @@
 package calculator
 
-import "math"
+import (
+	"math"
+)
 
 type CalculateTaxBody struct {
 	TotalIncome    float64     `json:"totalIncome" validate:"required,gte=0"`
@@ -11,6 +13,16 @@ type CalculateTaxBody struct {
 type Allowance struct {
 	Type   string  `json:"allowanceType"  example:"donation" validate:"required,oneof=donation k-receipt"`
 	Amount float64 `json:"amount" validate:"gte=0"`
+}
+
+type TaxLevel struct {
+	Level string  `json:"level"`
+	Tax   float64 `json:"tax"`
+}
+type CalculateTaxResponse struct {
+	Tax       float64    `json:"tax"`
+	TaxLevel  []TaxLevel `json:"taxLevel,omitempty"`
+	TaxRefund float64    `json:"taxRefund,omitempty"`
 }
 
 const (
@@ -44,6 +56,20 @@ func GetTotalTax(taxable float64) float64 {
 	return 0.0
 }
 
+func GetTaxLevels(taxable float64) (taxLevel []TaxLevel) {
+	taxLevel = append(taxLevel, TaxLevel{Level: "0-150,000", Tax: 0})
+	taxable = math.Max(taxable-150000, 0)
+	taxLevel = append(taxLevel, TaxLevel{Level: "150,001-500,000", Tax: (math.Min(taxable, 350000) * 0.10)})
+	taxable = math.Max(taxable-350000, 0)
+	taxLevel = append(taxLevel, TaxLevel{Level: "500,001-1,000,000", Tax: (math.Min(taxable, 500000) * 0.15)})
+	taxable = math.Max(taxable-500000, 0)
+	taxLevel = append(taxLevel, TaxLevel{Level: "1,000,001-2,000,000", Tax: (math.Min(taxable, 1000000) * 0.20)})
+	taxable = math.Max(taxable-1000000, 0)
+	taxLevel = append(taxLevel, TaxLevel{Level: "2,000,001 ขึ้นไป", Tax: (math.Min(taxable, 2000000) * 0.35)})
+
+	return taxLevel
+}
+
 func getDonationAllowance(allowances []Allowance) float64 {
 	donation := 0.0
 	maxDonation := 100000.0
@@ -57,12 +83,16 @@ func getDonationAllowance(allowances []Allowance) float64 {
 	return math.Min(donation, maxDonation)
 }
 
-func CalculateTax(b CalculateTaxBody) (tax, taxRefund float64) {
+func CalculateTax(b CalculateTaxBody) CalculateTaxResponse {
 	allowance := getDonationAllowance(b.Allowances)
-	tax = GetTotalTax(b.TotalIncome-PersonalDeduction-allowance) - b.WithHoldingTax
 
+	tax := GetTotalTax(b.TotalIncome-PersonalDeduction-allowance) - b.WithHoldingTax
+	roundedTax := math.Round(tax*100) / 100
+	var taxLevel []TaxLevel
 	if tax < 0 {
-		return 0, math.Abs(tax)
+		return CalculateTaxResponse{0, taxLevel, math.Abs(roundedTax)}
 	}
-	return tax, 0
+
+	taxLevel = GetTaxLevels(b.TotalIncome - allowance - PersonalDeduction)
+	return CalculateTaxResponse{math.Max(0, roundedTax), taxLevel, 0}
 }
